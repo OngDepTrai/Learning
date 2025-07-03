4454f309b56575959443102f7c9042b6bcc0e6f1
OngDepTrai <RoaLock@gmail.com>
2025-07-03 16:39:35 -0500

    Created "DecorderRing" for the Data Annotation code challenge

---
+++ DecorderRing.java
@@ -0,0 +1,23 @@
+
+/*This is for the Data Annotation coding challenge.  I've never done something like this.  
+It feels like it is targetted for Senior level software engineers.  At the very least a 
+good learning opportunity
+--------------------
+Challenge is to take a Google Doc from a User provided URL.  
+The document will be parsed and then function must print a grid of characters
+Should be printed in a fixed-width font
+The characters in the grid will form a graphic
+Which should show a sequence of uppercase letters
+The letters will reveal a secret message
+---------------------------------------------
+The grid uses X,Y coordinates
+min value is 0
+max value is not limited
+Any position in the grid that does not have a specified character should be filled w/ a "Space" character
+*/
+
+//Because the max value isn't limited something like an ArrayList seems easiest
+
+class DecoderRing {
+    
+}
